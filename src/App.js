c0b5fcddccc44e685679fa47c50d68c2c97cb911
Roland Khoury <devrolkh@gmail.com>
2021-12-29 13:25:23 +0200

    Added year, Added Pagination Integration

--- src/App.js
+++ src/App.js
@@ -1,54 +1,72 @@
 import React, {useEffect, useState} from 'react';
-
 import Movie from './components/Movie';
-import './App.css';
+import Pagination from '@mui/material/Pagination';
 
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5003d23dedc1001d745759e4c7ffe979&page=1";
+import './App.css';
+import { makeStyles } from '@mui/styles';
+
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5003d23dedc1001d745759e4c7ffe979&page=";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=5003d23dedc1001d745759e4c7ffe979&query=";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [page, setPage] = React.useState(1);
+  let moviesList = [];
 
   async function getMovies(url) {
     const moviesResp = await fetch(url)
-    const moviesList = await moviesResp.json()
+    moviesList = await moviesResp.json()
     setMovies(moviesList)
   }
 
   useEffect( () => {
+    if(searchTerm){
+      getMovies(SEARCHAPI + searchTerm + '&page=' + page);
+    }else{
+      getMovies(APIURL + page);
+    }
     
-    getMovies(APIURL);
     
-  }, [])
+  }, [page])
 
   console.log(movies)
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-
-    if(searchTerm) {
-      getMovies(SEARCHAPI + searchTerm);
-      setSearchTerm("");
-    }
-    
-  }
+  
 
   const handleOnChange = (e) => {
-    setSearchTerm(e.target.value)
+    setSearchTerm(e.target.value);
+    if(searchTerm) {
+      getMovies(SEARCHAPI + searchTerm);
+    }
+  }
+  
+  const handleOnPageChange = (event, value) => {
+    console.log(value)
+    setPage(value);
   }
 
+  const useStyles = makeStyles(() => ({
+    ul: {
+      "& .MuiPaginationItem-root": {
+        color: "#fff"
+      }
+    }
+  }));
+
+  const classes = useStyles();
   return (
     <>
       <header>
-        <form onSubmit={handleOnSubmit}>
-          <input className="search" type="search" placeholder='Search' valur={searchTerm} onChange={handleOnChange} />
-        </form>
+          <input className="search" type="search" placeholder='Search' value={searchTerm} onChange={handleOnChange} />
       </header>
       <div className='movie-container'>      
         {movies.results?.length > 0 && movies.results.map((movie) => (
           <Movie key={movie.id} {...movie}/>
         ))}
+      </div>
+      <div className="movie-pagination">
+        <Pagination classes={{ ul: classes.ul }} count={movies.total_pages < 500 ? (movies.total_pages) : 500} page={page ?? 1} onChange={handleOnPageChange} variant="outlined" color="primary" />
       </div>
     </>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,13 +13,15 @@ const setVoteClass = (vote_average) => {
 }
 
 
-const Movie = ({ title, poster_path, overview, vote_average}) => 
+const Movie = ({ title, poster_path, overview, release_date, vote_average}) => 
     <div className="movie">
         <img src={poster_path ? (IMGPATH + poster_path) : 'https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' } alt={title} />
         <div className="movie-info">
             <h3>{title}</h3>
             <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+            
         </div>
+        <div className="movie-info">{release_date ? release_date.split("-")[0] : ''}</div>
         <div className="movie-over">
             <h2>Overview</h2>
             <p>{overview}</p>
