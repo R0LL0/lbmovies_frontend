cb853fdb875123b741a8960ded4724644c1bd5f2
Roland Khoury <devrolkh@gmail.com>
2021-12-29 04:18:37 +0200

    UI Improvements, API Integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
 import Movie from './components/Movie';
+import './App.css';
+
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5003d23dedc1001d745759e4c7ffe979&page=1";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=5003d23dedc1001d745759e4c7ffe979&query=";
 
 function App() {
-  const movies = ['Movie 1', 'Movie 2', 'Movie 3'];
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  async function getMovies(url) {
+    const moviesResp = await fetch(url)
+    const moviesList = await moviesResp.json()
+    setMovies(moviesList)
+  }
+
+  useEffect( () => {
+    
+    getMovies(APIURL);
+    
+  }, [])
+
+  console.log(movies)
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCHAPI + searchTerm);
+      setSearchTerm("");
+    }
+    
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
-    <div>
-      {movies.map((movie) => (
-        <Movie />
-      ))}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input className="search" type="search" placeholder='Search' valur={searchTerm} onChange={handleOnChange} />
+        </form>
+      </header>
+      <div className='movie-container'>      
+        {movies.results?.length > 0 && movies.results.map((movie) => (
+          <Movie key={movie.id} {...movie}/>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-const Movie = () => 
+const setVoteClass = (vote_average) => {
+    if(vote_average >= 8){
+        return 'green'
+    }else if(vote_average >= 6){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+}
+
+
+const Movie = ({ title, poster_path, overview, vote_average}) => 
     <div className="movie">
-        This is a movie component
+        <img src={poster_path ? (IMGPATH + poster_path) : 'https://images.unsplash.com/photo-1560109947-543149eceb16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' } alt={title} />
+        <div className="movie-info">
+            <h3>{title}</h3>
+            <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+        </div>
+        <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+        </div>
     </div>;
 
 
