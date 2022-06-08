7000ab58371e95e7c46bc31d682d91734f1d6a7a
Roland K <devrolkh@gmail.com>
2022-06-09 02:20:19 +0300

    date update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -21,7 +21,7 @@ const Movie = ({ title, poster_path, overview, release_date, vote_average}) =>
             <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
             
         </div>
-        <div className="movie-info">{release_date ? release_date.split("-")[0] : ''}</div>
+        <div className="movie-info">{release_date ? release_date.replace("-", "/") : ''}</div>
         <div className="movie-over">
             <h2>Overview</h2>
             <p>{overview}</p>
