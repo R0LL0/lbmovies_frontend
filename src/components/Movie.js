8217c54bfe62e4f58a381cd50390b2e4b15da3e7
Roland Khoury <roland.khoury@dataprime-solutions.com>
2025-12-14 02:11:32 +0200

    Fix missing useEffect import in Movie and Serie components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w500"; // Using w500 for better performance
